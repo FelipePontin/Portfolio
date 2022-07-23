e2a9a0fbda0254af0394aa2b666287ad760fed0a
FelipePontin <felipepan64@gmail.com>
2022-07-23 14:16:36 -0300

    att

--- assets/js/linguagens.js
+++ assets/js/linguagens.js
@@ -1,8 +1,18 @@
-const btnsSetaLinguagem = document.querySelectorAll(".botao-linguagem")
+const cards = document.querySelectorAll(".card")
 
-btnsSetaLinguagem.forEach( btnSetaLinguagem => {
-    btnSetaLinguagem.addEventListener("click", () => {
-        const resumoLinguagem = btnSetaLinguagem.nextElementSibling
-        resumoLinguagem.classList.toggle("resumoOpen")
+// const shadows = [
+//     "#e44c2228 0px 2px 4px 0px, #e44c2228 0px 2px 16px 0px",
+//     "#2552e828 0px 2px 4px 0px, #2552e828 0px 2px 16px 0px",
+//     "#cc649b28 0px 2px 4px 0px, #cc649b28 0px 2px 16px 0px",
+//     "#f3e31b28 0px 2px 4px 0px, #f3e31b28 0px 2px 16px 0px",
+//     "#3474a628 0px 2px 4px 0px, #3474a628 0px 2px 16px 0px"
+// ]
+
+cards.forEach((card, selecionado) => {
+    card.addEventListener("click", () => {
+        const resumo = card.lastElementChild
+        resumo.classList.toggle("resumoOpen")
+        // card.style.boxShadow = shadows[selecionado]
+        // card.style.boxShadow = "transparent"
     })
 })
